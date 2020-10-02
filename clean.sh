0ba6bb2b5f14f8cf320e123898fb5ef9eed9d880
Richard Anderson <rich@pop-os.localdomain>
2020-10-01 21:37:12 -0500

    fix cleanup

--- clean.sh
+++ clean.sh
@@ -4,6 +4,10 @@ if lsmod |grep -q g_mass_storage; then
     rmmod g_mass_storage
 fi
 
+if lsmod |grep -q g_ether; then
+    rmmod g_ether
+fi
+
 if mountpoint -q /iso; then
     umount "$(losetup -PLf /iso.img --show)p1"
 fi
--- mode.sh
+++ mode.sh
@@ -8,17 +8,11 @@ MODE=${1:-hdd}
 source "$IDIR/clean.sh"
 
 if [ "$MODE" == "hdd" ] ; then
-    if $(lsmod | grep g_ether); then
-        rmmod g_ether
-    fi
     modprobe g_mass_storage file=/iso.img luns=1 stall=0 ro=0 cdrom=0 removable=1
 elif [ "$MODE" == "cd" ] ; then
     mount -o ro "$(losetup -PLf /iso.img --show)p1" /iso
 elif [ "$MODE" == "usb" ] ; then
     mount "$(losetup -PLf /iso.img --show)p1" /iso
 elif [ "$MODE" == "net" ] ; then
-    if $(lsmod | grep g_mass_storage) ; then
-        rmmod g_mass_storage
-    fi
     modprobe g_ether
 fi
