e20e21c3a0a14bb7315c456f4fc9dee054836a42
Richard Anderson <rich@pop-os.localdomain>
2020-10-01 21:27:52 -0500

    if condition for when modules are loaded

--- mode.sh
+++ mode.sh
@@ -8,11 +8,17 @@ MODE=${1:-hdd}
 source "$IDIR/clean.sh"
 
 if [ "$MODE" == "hdd" ] ; then
-    rmmod g_ether && modprobe g_mass_storage file=/iso.img luns=1 stall=0 ro=0 cdrom=0 removable=1
+    if $(lsmod | grep g_ether); then
+        rmmod g_ether
+    fi
+    modprobe g_mass_storage file=/iso.img luns=1 stall=0 ro=0 cdrom=0 removable=1
 elif [ "$MODE" == "cd" ] ; then
     mount -o ro "$(losetup -PLf /iso.img --show)p1" /iso
 elif [ "$MODE" == "usb" ] ; then
     mount "$(losetup -PLf /iso.img --show)p1" /iso
 elif [ "$MODE" == "net" ] ; then
-    rmmod g_mass_storage && modprobe g_ether
+    if $(lsmod | grep g_mass_storage) ; then
+        rmmod g_mass_storage
+    fi
+    modprobe g_ether
 fi
