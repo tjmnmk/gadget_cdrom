82f74dbec6c2d437e23ef2f58a86eee28eb6d39a
Richard Anderson <rich@pop-os.localdomain>
2020-10-03 17:20:47 -0500

    add sync to cleanup

--- clean.sh
+++ clean.sh
@@ -1,5 +1,9 @@
 #! /bin/bash -eu
 
+## Not a garuntee this will prevent issues if copying files while chaging modes
+## but should help
+sync
+
 if lsmod |grep -q g_mass_storage; then
     rmmod g_mass_storage
 fi
