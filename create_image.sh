49e822ca8b7f0f113d2db13535499d6238bf1078
Adam Bambuch <adam.bambuch2@gmail.com>
2021-02-23 01:11:10 +0100

    Revert "Revert "create_image.sh auto - guess iso.img size""
    This reverts commit 633b3153ea76bdbcc8505587761cc8d282fc237a.

--- create_image.sh
+++ create_image.sh
@@ -1,17 +1,36 @@
 #!/bin/bash -ue
 
-FILE="/iso.img"
-
-if [ -f "$FILE" ] ; then
-    echo "$FILE already exists"
-    exit 1
+auto=0
+if [ "$#" -eq 1 ] && [ "$1" = "auto" ]; then
+    auto=1
 fi
 
-free="$(df -h / | tail -n1 | awk '{print $4}')"
-echo -ne "Space available: $free\nSize, e.g. 16G? "
-read size
+FILE="/iso.img"
 
+if [ -f "$FILE" ]; then
+    if [ "$auto" -eq 0 ]; then
+        echo "$FILE already exists"
+        exit 1
+    fi
+    exit 0
+fi
+
+size=0
+if [ "$auto" -eq 0 ]; then
+    free="$(df -h / | tail -n1 | awk '{print $4}')"
+    echo -ne "Space available: $free\nSize, e.g. 16G? "
+    read size
+else
+    free="$(df -k / | tail -n1 | awk '{print $4}')"
+    size=$(($free-(1024*1024*2)))
+    if [ "$size" -lt "$((free/2))" ]; then
+        size=$((free/2))
+    fi
+    size="${size}k"
+fi
+ 
 echo "Creating $size image..."
+
 fallocate -l "$size" "$FILE"
 dev="$(losetup -fL --show "$FILE")"
 parted "$dev" mklabel msdos
